499e69b9d1414136ef62f8e7c1bc9eabc47fa768
AlaaAlkhaled <alaa.alkhaled7@gmail.com>
2024-08-30 14:23:26 +0200

    first assignment 0.1
    Adding the two recreations of an art

---
+++ experiments/art.js
@@ -0,0 +1,62 @@
+const gridSize = 10;
+const variation = 3;
+const direction = 0.1;
+const numBands = 20;
+
+function setup() {
+    createCanvas(innerHeight, innerWidth);
+    noLoop(); 
+}
+
+function draw() {
+    background(245, 240, 235);
+    circles = [];
+
+    let bandWidth = width / numBands;
+    // next lines are created with help of chatGPT to for the direction af the taper 
+    for (let y = 0; y < height; y += gridSize) {
+        let taperAmount = calculateTaper(y); 
+        let xStart = taperAmount / 2;
+
+    for (let band = 0; band < numBands; band++) {
+        let bandColor = getColor(band); 
+        fill(bandColor);
+        noStroke();
+
+      // this part where created with help of ChatGPT, Loop through each cell in the current band
+    for (let x = xStart + band * bandWidth; x < xStart + (band + 1) * bandWidth; x += gridSize) {
+        let size = random(gridSize - variation, gridSize + variation); // Random circle size
+        let xOffset = random(-variation, variation); // Random horizontal offset
+        let yOffset = random(-variation, variation); // Random vertical offset
+        let posX = x + xOffset;
+        let posY = y + yOffset;
+
+    if (!isOverlapping(posX, posY, size / 2)) {
+        ellipse(posX, posY, size);
+        circles.push({ x: posX, y: posY, r: size / 2 });
+        }
+    }
+    }
+}
+}
+
+function calculateTaper(y) {
+    let taper = (y / height) * (width * direction);
+    return taper;
+}
+
+function getColor(band) {
+    let colors = ['#F28E36', '#D8352A', '#35477D', '#45B8AC', '#F5E663'];
+    return colors[band % colors.length];
+}
+
+function isOverlapping(x, y, r) {
+    for (let i = 0; i < circles.length; i++) {
+        let other = circles[i];
+        let distance = dist(x, y, other.x, other.y);
+    if (distance < r + other.r) {
+        return true;
+    }
+}
+return false;
+}
---
+++ experiments/art1.js
@@ -0,0 +1,35 @@
+let numLayers = 10;
+let layerHeight;
+let noiseScale = 0.003; 
+let noiseStrength = 100; 
+
+function setup() {
+    createCanvas(600, 800);
+    noLoop();
+
+    layerHeight = height / numLayers + 50;
+}
+
+function draw() {
+    background(245, 240, 235);
+
+    for (let i = 0; i < numLayers; i++) {
+    let yOffset = i * layerHeight;
+    let colorValue = map(i, 0, numLayers, 0, 255);
+    fill(colorValue);
+    noStroke();
+
+    let noiseOffset = random(0, 1000);
+
+    beginShape();
+    vertex(0, height);
+    for (let x = 0; x <= width; x += 5) {
+      // next lines are created with help of chatGPT to make the layers different from each other
+      let y = noise((x + noiseOffset) * noiseScale, i * noiseScale) * noiseStrength + yOffset;
+        vertex(x, y);
+    }
+    vertex(width, height);
+    endShape(CLOSE);
+    }
+    
+}
